28f2013661b1fcd04aea2847ca4952f1b414e974
bwpge <bwpge.dev@gmail.com>
2025-08-03 21:38:39 -0400

    Update color test script

--- colortest.sh
+++ colortest.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-for fg_color in {30..37} {90..97}; do
-    for bg_color in {40..47} {100..107}; do
-        echo -ne "\e[${fg_color};${bg_color}m  \e[0m"
+for i in {0..15}; do
+    printf "\e[48;5;${i}m %3s \e[0m" "$i"
+    if (( (i + 1) % 8 == 0 )); then
+        echo
+    fi
+done
+echo
+
+for i in {16..231}; do
+    printf "\e[48;5;${i}m %3s \e[0m" "$i"
+    if (( (i - 15) % 6 == 0 )); then
+        echo
+    fi
+done
+echo
+
+for i in {232..255}; do
+    printf "\e[48;5;${i}m %3s \e[0m" "$i"
+    if (( (i - 15) % 6 == 0 )); then
+        echo
+    fi
+done
+echo
+
+for fg in {30..37}; do
+    for bg in {40..47}; do
+        printf "\e[${fg};${bg}m fg${fg}-bg${bg} \e[0m  "
     done
     echo
 done
-
-# echo -e "\nBasic formatting:"
-# echo -e "\e[1mBold Text\e[0m"
-# echo -e "\e[4mUnderlined Text\e[0m"
-# echo -e "\e[5mBlinking Text (may not work on all terminals)\e[0m"
-# echo -e "\e[7mInverted Text\e[0m"
-# echo -e "\e[8mHidden Text (select to reveal)\e[0m"
-# echo -e "\e[0m" # Reset all attributes
